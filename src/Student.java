41db7214740acce3235e021d36dc4bad3669e589
shagunbhuyan29 <shagun.25bai11614@vitbhopal.ac.in>
2026-09-10 14:43:14 +0530

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,1 +1,45 @@
+public class Student extends Person {
 
+    private int studentId;
+    private String branch;
+    private int semester;
+
+    // Constructor
+    public Student(int studentId, String name, String branch, int semester) {
+        super(name);
+        this.studentId = studentId;
+        this.branch = branch;
+        this.semester = semester;
+    }
+
+    // Getter methods
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    // Setter methods
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    // Method overriding
+    @Override
+    public void displayInfo() {
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Name: " + getName());
+        System.out.println("Branch: " + branch);
+        System.out.println("Semester: " + semester);
+    }
+}
