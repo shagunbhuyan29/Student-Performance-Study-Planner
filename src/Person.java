6d985272311d9a8835917284857718ed690da1ef
shagunbhuyan29 <shagun.25bai11614@vitbhopal.ac.in>
2026-09-10 14:31:05 +0530

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,24 @@
+public class Person {
 
+    private String name;
+
+    // Constructor
+    public Person(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Method that can be overridden
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+    }
+}
